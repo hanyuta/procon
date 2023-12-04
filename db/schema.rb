2bb250f79975c4a953da95193c1715b87c0f5e4f
hanyuta <hanyutatech24@gmail.com>
2023-12-04 16:03:43 +0900

    usersテーブルカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_04_021102) do
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.boolean "admin_true", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
