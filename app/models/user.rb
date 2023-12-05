8d71422445fd152dcee94d36d4b9d402e8a60cc3
hanyuta <hanyutatech24@gmail.com>
2023-12-06 00:20:51 +0900

    バリデーション、アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessor :admin_password
+
+  has_one :clients_info_name
+  has_many :schedules
+  has_many :comments
+
+  validates :password,    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合を混同して設定してください' }
+
+  validates :first_name,  presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください'}
+  validates :last_name,   presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角ひらがな、カタカナ、漢字で入力してください'}
+
 end
