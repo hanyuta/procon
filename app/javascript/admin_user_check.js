56ea8773aed7afc66d5b33e27ec646ce78849813
hanyuta <hanyutatech24@gmail.com>
2023-12-06 00:19:55 +0900

    Update admin_user_check.js

--- app/javascript/admin_user_check.js
+++ app/javascript/admin_user_check.js
@@ -2,14 +2,12 @@ function AdminPasswordFrom(){
     const adminCheckbox= document.getElementById('admin-checkbox');
     const changeInputForm=document.getElementById('admin-user-check');
 
-    adminCheckbox.addEventListener('change',function(){
         const whichStyle= changeInputForm.getAttribute("style")
         if(whichStyle == "display:none;"){
             changeInputForm.setAttribute("style","display:block;");
         }else if(whichStyle == "display:block;"){
             changeInputForm.setAttribute("style","display:none;");
         };
-    });
 };
 
-window.addEventListener("load",AdminPasswordFrom)
+window.addEventListener("change",AdminPasswordFrom)
