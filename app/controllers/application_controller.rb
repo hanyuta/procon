28c45ed1c10ae171c640122104b5b60c794ecdb8
hanyuta <hanyutatech24@gmail.com>
2023-12-06 00:21:34 +0900

    sanitizer記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   private 
 
   def configure_permitted_parameters
-    # devise_parameter_sanitizer.permit(:sign_up, keys: [:])
+    if params[:user].present? && params[:user][:admin_password].present?
+      admin_password = params[:user][:admin_password]
+
+      if admin_password == ENV["ADMIN_USER_PASSWORD"]
+        admin_true = true
+      end
+    else
+      admin_true = false
+    end
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name , :last_name ,:admin_true])
   end
 
   def basic_auth
