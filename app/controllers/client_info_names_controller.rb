1f6514b4444144e306c58f8649ec534f8691a80e
hanyuta <hanyutatech24@gmail.com>
2023-12-09 17:26:17 +0900

    顧客情報名称登録アクション(create)

--- app/controllers/client_info_names_controller.rb
+++ app/controllers/client_info_names_controller.rb
@@ -6,6 +6,16 @@ class ClientInfoNamesController < ApplicationController
     end
 
     def create
-        
+        @client_info_name = ClientsInfoName.new(client_info_params)
+        @client_info_name.save
+        redirect_to root_path
     end
+
+    private
+
+    def client_info_params
+        params.require(:clients_info_name).permit(:clients_info1_name,:clients_info2_name,:clients_info3_name,:clients_info4_name,:clients_info5_name,
+                                                :clients_info6_name,:clients_info7_name,:clients_info8_name,:clients_info9_name,:clients_info10_name ).merge(user_id: current_user.id)
+    end
+
 end
