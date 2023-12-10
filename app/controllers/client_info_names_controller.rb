8ef23547da3bc3338fd46ce7dec1099fa2d8902a
hanyuta <hanyutatech24@gmail.com>
2023-12-10 19:10:47 +0900

    Update client_info_names_controller.rb

--- app/controllers/client_info_names_controller.rb
+++ app/controllers/client_info_names_controller.rb
@@ -2,7 +2,7 @@ class ClientInfoNamesController < ApplicationController
     before_action :authenticate_user!, expect: [:index]
     
     def index
-        @client_info_name = ClientsInfoName.new
+
     end
 
     def create
