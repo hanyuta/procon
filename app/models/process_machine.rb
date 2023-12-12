726fe61827f16da2800c66ad0ec9e8c09a861f0c
hanyuta <hanyutatech24@gmail.com>
2023-12-12 11:31:32 +0900

    アソシエーション、バリデーション設定

--- app/models/process_machine.rb
+++ app/models/process_machine.rb
@@ -1,2 +1,9 @@
 class ProcessMachine < ApplicationRecord
+
+    has_many :process_schedules
+
+    validates :pm_name,                    presence: true 
+    validates :pm_abbreviation,            presence: true ,length: { maximum: 5, message: "は5文字以内で入力してください" }
+    validates :pm_color,                   presence: true 
+
 end
