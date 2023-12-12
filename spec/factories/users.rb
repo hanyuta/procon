21a48da6924191ee9bd861d84ce840a216b83d06
hanyuta <hanyutatech24@gmail.com>
2023-12-12 13:08:13 +0900

    Factory.Bot設定、修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     email                 { Faker::Internet.email }
     password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
-    first_name           { '太郎' }
+    first_name            { '太郎' }
     last_name             { '田中' }
+    admin_true            {  true  }
   end
 end
