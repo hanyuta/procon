67eec845b37a6b6c48f39a43ad16c75fe34600ab
hanyuta <hanyutatech24@gmail.com>
2023-12-18 17:09:01 +0900

    Update schedules_controller.rb

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,8 +1,18 @@
 class SchedulesController < ApplicationController
+  before_action :authenticate_user!, expect: [:new]
+  before_action :set_client_info_names, expect: [:index , :new]
 
   def index
-    @client_info_name = ClientsInfoName.first
   end
 
+  def new
+    @process_machine = ProcessMachine.all
+  end
+
+  private
+
+  def set_client_info_names
+    @client_info_name = ClientsInfoName.first
+  end
 
 end
