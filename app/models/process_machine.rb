b0e2f991c76d1cb602848b5b49e2c426c664dbdc
hanyuta <hanyutatech24@gmail.com>
2023-12-19 16:19:49 +0900

    Update process_machine.rb

--- app/models/process_machine.rb
+++ app/models/process_machine.rb
@@ -1,6 +1,6 @@
 class ProcessMachine < ApplicationRecord
 
-    has_many :process_schedules
+    has_one :schedule
 
     validates :pm_name,                    presence: true 
     validates :pm_abbreviation,            presence: true ,length: { maximum: 5, message: "は5文字以内で入力してください" }
