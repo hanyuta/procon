e444d6c6b6165d103ad03e0743179d8b9fd89c66
hanyuta <hanyutatech24@gmail.com>
2023-12-19 16:36:05 +0900

    Update schedule.rb

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,6 @@
 class Schedule < ApplicationRecord
-    belongs_to: client_info
-    belongs_to: process_machine
+    belongs_to :client_info
+    belongs_to :process_machine
 
     validates :start_date , presence: true 
     validates :end_date   , presence: true 
