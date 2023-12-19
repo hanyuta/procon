832894ac3991f321a702a83c75e60fb69a3cf5aa
hanyuta <hanyutatech24@gmail.com>
2023-12-20 07:27:17 +0900

    Update clients_info_controller.rb

--- app/controllers/clients_info_controller.rb
+++ app/controllers/clients_info_controller.rb
@@ -1,14 +1,25 @@
 class ClientsInfoController < ApplicationController
 
     def index
+        @client_info_name = ClientsInfoName.first
         @client_info = ClientInfo.new
     end
 
     def create
+        @client_info = ClientInfo.new(client_name_params)
 
+        if @client_info.save
+            redirect_to clients_info_index_path 
+        else
+            render :index, status: :unprocessable_entity
+        end
     end
 
     private
 
+    def client_name_params
+        params.require(:client_info).permit(:client_name,:client_info1,:client_info2,:client_info3,:client_info4,:client_info5,
+                                                :client_info6,:client_info7,:client_info8,:client_info9,:client_info10 )
+    end
 
 end
