d249fc0f959e1e2bc26a677257af14a3e2b4ef97
hanyuta <hanyutatech24@gmail.com>
2023-12-20 17:23:29 +0900

    新規登録顧客情報登録画面実装

--- app/controllers/clients_info_controller.rb
+++ app/controllers/clients_info_controller.rb
@@ -6,10 +6,11 @@ class ClientsInfoController < ApplicationController
     end
 
     def create
+        @client_info_name = ClientsInfoName.first
         @client_info = ClientInfo.new(client_name_params)
 
         if @client_info.save
-            redirect_to clients_info_index_path 
+            redirect_to new_schedule_path
         else
             render :index, status: :unprocessable_entity
         end
@@ -19,7 +20,7 @@ class ClientsInfoController < ApplicationController
 
     def client_name_params
         params.require(:client_info).permit(:client_name,:client_info1,:client_info2,:client_info3,:client_info4,:client_info5,
-                                                :client_info6,:client_info7,:client_info8,:client_info9,:client_info10 )
+                                                :client_info6,:client_info7,:client_info8,:client_info9,:client_info10 ).merge(clients_info_name_id: @client_info_name.id)
     end
 
 end
