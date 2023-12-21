28b0aa832930153e97314be3cab1e6f3341c8155
hanyuta <hanyutatech24@gmail.com>
2023-12-22 07:14:19 +0900

    コメント欄（フローティングウィンドウ）のJS追加

---
+++ app/javascript/comment_bar.js
@@ -0,0 +1,14 @@
+function CommentBar(){
+  document.getElementById('openButton').addEventListener('click', function() {
+    var sidebar = document.getElementById('sidebar');
+    if (sidebar.style.width == '0px' || sidebar.style.width == '') {
+      sidebar.style.width = '350px';
+      sidebar.style.right = '0';
+    } else {
+      sidebar.style.width = '0';
+      sidebar.style.right = '-350px';
+    }
+  });
+}
+
+window.addEventListener("DOMContentLoaded",CommentBar)
