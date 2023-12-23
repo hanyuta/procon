77a15d03aacf61559aa132b2643b510cb796cffe
hanyuta <hanyutatech24@gmail.com>
2023-12-23 14:52:07 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_034611) do
   end
 
   create_table "comments", charset: "utf8", force: :cascade do |t|
-    t.string "comment", null: false
+    t.string "comment_data", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
