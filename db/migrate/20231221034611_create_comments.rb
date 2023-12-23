9a6172e8d0d91696aa8fc0cf427b8ae19d47cb7c
hanyuta <hanyutatech24@gmail.com>
2023-12-23 14:51:04 +0900

    Update 20231221034611_create_comments.rb

--- db/migrate/20231221034611_create_comments.rb
+++ db/migrate/20231221034611_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string      :comment   ,null:false
-      t.references  :user      ,null:false , foreign_key:true
+      t.string      :comment_data   ,null:false
+      t.references  :user           ,null:false , foreign_key:true
       t.timestamps
     end
   end
