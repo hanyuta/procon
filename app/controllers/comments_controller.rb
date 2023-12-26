1c77810415bdc2fa0d30c73a611a736a17c1dd42
hanyuta <hanyutatech24@gmail.com>
2023-12-27 07:53:18 +0900

    顧客情報名称が設定されていない場合の記述修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
     def create
-        @comment = Comment.create(comment_params)
-        render json:{comment: @comment}
+        @comment = Comment.new(comment_params)
+        if @comment.save
+          render json: { comment: @comment }
+        else
+          render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     private
