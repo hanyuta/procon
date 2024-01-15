6d2320ab567afab5349a5a8791f032e798c39c25
hanyuta <hanyutatech24@gmail.com>
2024-01-15 22:53:39 +0900

    バリデーション、アソシエーション変更

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,6 @@
 class Schedule < ApplicationRecord
-    belongs_to :client_info
-    belongs_to :process_machine
-
     validates :start_date , presence: true 
     validates :end_date   , presence: true 
+    validates :client_info_id ,presence: true
+    validates :process_machine_id , presence: true
 end
